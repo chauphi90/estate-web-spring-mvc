85bf240f8d39c90fc3404965e3fc73a440b509a7
chauphi90 <chauphi90@gmail.com>
2020-06-14 16:54:28 +0700

    Revert "Edit configuration files for running"
    This reverts commit 091980b9c7af7af28ca02e79090c75a19956b6f0.
    
    # Conflicts:
    #	src/main/java/com/laptrinhjavaweb/controller/web/HomeController.java
    #	src/main/resources/application.properties
    #	src/main/webapp/WEB-INF/web.xml

--- src/main/java/com/laptrinhjavaweb/SpringBootWebApplication.java
+++ src/main/java/com/laptrinhjavaweb/SpringBootWebApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-
-import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringBootWebApplication extends SpringBootServletInitializer {
@@ -22,10 +18,4 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 
-	@Bean
-	public FilterRegistrationBean siteMeshFilter() {
-		FilterRegistrationBean filter = new FilterRegistrationBean();
-		filter.setFilter(new SiteMeshFilter());
-		return filter;
-	}
 }
