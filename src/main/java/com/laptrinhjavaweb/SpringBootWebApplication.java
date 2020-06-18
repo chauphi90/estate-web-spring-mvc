118e8c9307380817ff8e313d1c69322d6f89fc1a
chauphi90 <chauphi90@gmail.com>
2020-06-18 20:13:11 +0700

    - Fix CSS importation page login - Remove main method in class spring booot application

--- src/main/java/com/laptrinhjavaweb/SpringBootWebApplication.java
+++ src/main/java/com/laptrinhjavaweb/SpringBootWebApplication.java
@@ -1,6 +1,5 @@
 package com.laptrinhjavaweb;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -14,8 +13,8 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 		return application.sources(SpringBootWebApplication.class);
 	}
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootWebApplication.class, args);
-	}
+//	public static void main(String[] args) throws Exception {
+//		SpringApplication.run(SpringBootWebApplication.class, args);
+//	}
 
 }
